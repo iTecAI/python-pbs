eb3fdb4ea3dc178084c33279172141ebd2743d6f
Dax Harris <dharr@lle.rochester.edu>
2024-01-25 08:52:48 -0500

    Scheduler model

--- python_pbs/pbs/models/__init__.py
+++ python_pbs/pbs/models/__init__.py
@@ -1,1 +1,2 @@
 from .server import Server, ServerLicenseCount, ServerStateCount
+from .scheduler import Scheduler
---
+++ python_pbs/pbs/models/scheduler.py
@@ -0,0 +1,27 @@
+from typing import Literal, Optional
+from pydantic import BaseModel
+
+
+class Scheduler(BaseModel):
+    id: Optional[str] = None
+    comment: Optional[str] = None
+    do_not_span_psets: Optional[bool] = False
+    job_sort_formula_threshold: Optional[float] = None
+    log_events: Optional[int] = 767
+    only_explicit_psets: Optional[bool] = False
+    opt_backfill_fuzzy: Optional[Literal["off", "low", "medium", "high"]] = "low"
+    partition: Optional[str] = None
+    pbs_version: Optional[str] = None
+    preempt_order: Optional[str] = "SCR"
+    preempt_prio: Optional[str] = "express_queue, normal_jobs"
+    preempt_queue_prio: Optional[int] = 150
+    preempt_sort: Optional[Literal["min_time_since_start"]] = "min_time_since_start"
+    scheduler_iteration: Optional[int] = 600
+    scheduling: Optional[bool] = False
+    sched_cycle_length: Optional[int] = 1200
+    sched_host: Optional[str] = None
+    sched_log: Optional[str] = None
+    sched_preempt_enforce_resumption: Optional[bool] = False
+    sched_priv: Optional[str] = None
+    state: Optional[Literal["down", "idle", "scheduling"]] = "down"
+    throughput_mode: Optional[bool] = True
