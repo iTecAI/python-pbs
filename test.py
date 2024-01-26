b62509c49cae5eb3ad5820139ab7921830e6b49c
Dax Harris <dharr@lle.rochester.edu>
2024-01-26 11:38:20 -0500

    wip testing

--- test.py
+++ test.py
@@ -4,23 +4,7 @@ from python_pbs.pbs.models.job import JobKeepFiles
 from python_pbs.util import *
 
 pbs = PBS()
-"""print(pbs.server.data)
-print(pbs.queues["workq"].data)
-print(pbs.nodes["hulkling"].data)
-print(pbs.schedulers["default"].data)
-print(
-    pbs.jobs.select(
-        criteria=[JobAttribute(name="job_state", value="F")], include_historical=True
-    )[0].data
-)"""
-result = pbs.submit_script(
-    "test_script.sh",
-    queue="workq",
-    name="curl-test",
-    output_directory="./test-out/curl-test",
-)
-if result:
-    print(result.data)
-    time.sleep(2)
-    for line in result.logs():
-        print(line.strip())
+print(pbs.server.data)
+print(pbs.queues.all[0].data)
+print(pbs.nodes.all[0].data)
+# print(stat_resource(pbs.connection, id="cput"))
